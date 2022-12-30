b7cf2339086b057177f2428b54f6d73b6902d33a
Арсений Блинов <arseniyblinov@MacBook-Air-Arsenij.local>
2022-12-30 12:31:24 +0400

    forth task from lesson1

---
+++ homework1/Task4.py
@@ -0,0 +1,8 @@
+num = int(input('Введите целое положительное число  '))
+max = num % 10
+while num >= 1:
+    num = num // 10
+    if num % 10 > max:
+        max = num % 10
+print('Максимальное число', max)
+
